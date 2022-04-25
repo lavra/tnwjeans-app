94289e72086622512145163af3bc2d249bfdaec7
Luiz Anselmo <lavra2021@gmail.com>
2022-04-24 23:47:14 -0300

    Modulos de Login e recuperação de senha

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -28,7 +28,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-button>
-                    {{ __('Link de redefinição de senha por e-mail') }}
+                    {{ __('Recuperar Senha') }}
                 </x-button>
             </div>
         </form>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,24 @@
 <?php
 
-use App\Http\Controllers\Admin\{
-    UserController,
-};
-
-use App\Http\Controllers\Web\{
-    HomeController,
-};
-
-
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Admin
+| Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
 */
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-/*
-|--------------------------------------------------------------------------
-| Site
-|--------------------------------------------------------------------------
-*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
