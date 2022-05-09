bae55b13a86b7180a5b0e721be29199f44da0299
Luiz Anselmo <lavra2021@gmail.com>
2022-05-09 11:00:22 -0300

    Tamanho das imagens no celular

--- app/Http/Requests/UploadSlider.php
+++ app/Http/Requests/UploadSlider.php
@@ -23,13 +23,13 @@ class UploadSlider extends FormRequest
      */
     public function rules()
     {
-        if ($this->page === 1) {
+        if ($this->page == 1) {
             $validate_img_put = 'image|mimes:jpeg|dimensions:min_width=1920,min_height:1280';
             $validate_img_post = 'required|image|mimes:jpeg|dimensions:min_width=1920,min_height:1280';
 
         } else {
-            $validate_img_post = 'required|image|mimes:jpeg|dimensions:min_width=700,min_height:1050';
-            $validate_img_put = 'image|mimes:jpeg|dimensions:min_width=700,min_height:1050';
+            $validate_img_post = 'required|image|mimes:jpeg|dimensions:min_width=707,min_height:1000';
+            $validate_img_put = 'image|mimes:jpeg|dimensions:min_width=707,min_height:1000';
         }
 
 
